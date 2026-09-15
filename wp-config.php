3f3a638800c0447c0e46929137ccb2296a927f23
Vup-Vup <vup0162@gmail.com>
2026-09-15 15:31:20 +0700

    lap2

--- wp-config.php
+++ wp-config.php
@@ -89,7 +89,8 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-
+define('WP_HOME', 'http://wordpress.local');
+define('WP_SITEURL', 'http://wordpress.local');
 
 /* That's all, stop editing! Happy publishing. */
 
